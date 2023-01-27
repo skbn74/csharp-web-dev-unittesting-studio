bd6f381bebba211d1559dad8d1772ab43ec84132
Sally Steuterman <sally.steuterman@gmail.com>
2023-01-27 10:43:38 -0600

    Updating studio to reflect new build practices

---
+++ BalancedBracketsTests/BalancedBracketsTests.cs
@@ -0,0 +1,13 @@
+﻿namespace BalancedBracketsTests;
+
+[TestClass]
+public class BalancedBracketsTests
+{
+    // TODO: Add tests to this file.
+
+    [TestMethod]
+    public void EmptyTest()
+    {
+        Assert.AreEqual(true, true);
+    }
+}
